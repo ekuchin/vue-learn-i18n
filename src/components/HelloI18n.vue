8a1e634af26a9c82f06435babc40cb75cb26b061
Eugene A. Kuchin <kea@project-osnova.ru>
2021-04-15 18:39:28 +0500

    Plural & Format ready

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -1,39 +1,43 @@
 <template>
-
-  <p>{{ t("cat") }}</p>
+  <p>{{ t("cat", { name: "Мурка" }) }}</p>
   <p>{{ t("dog") }}</p>
   <p>{{ t("horse") }}</p>
   <p>{{ t("crocodile") }}</p>
-  
-    <button @click="setLocale('en')">English</button>
-    <button @click="setLocale('ru')">Русский</button>
+  <p>{{ t("email", { email }) }}</p>
 
+  <p>{{ t("person", 0) }}</p>
+  <p>{{ t("person", 1) }}</p>
+  <p>{{ t("person", 2) }}</p>
+
+  <button @click="setLocale('en')">English</button>
+  <button @click="setLocale('ru')">Русский</button>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { useI18n } from "vue-i18n";
+  import { defineComponent } from "vue";
+  import { useI18n } from "vue-i18n";
 
-export default defineComponent({
-  name: "HelloI18n",
-  setup() {
-    
-    const { t, locale } = useI18n({
-      inheritLocale: true,
-      useScope: "global",
-    });
+  export default defineComponent({
+    name: "HelloI18n",
+    setup() {
+      const { t, tc, locale } = useI18n({
+        inheritLocale: true,
+        useScope: "global",
+      });
 
-    const setLocale = (code) =>{
-      locale.value = code
-      localStorage.setItem("locale",code)
-    }
+      const setLocale = (code) => {
+        locale.value = code;
+        localStorage.setItem("locale", code);
+      };
 
-    locale.value = localStorage.getItem("locale")
-    if (!locale.value){
-      setLocale(navigator.language)
-    }
+      const email = "me@example.ru";
 
-    return { t, locale, setLocale };
-  },
-});
+      locale.value = localStorage.getItem("locale");
+      if (!locale.value) {
+        setLocale(navigator.language);
+      }
+
+      return { t, tc, locale, setLocale, email };
+    },
+  });
 </script>
